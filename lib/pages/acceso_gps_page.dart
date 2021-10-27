d5c61dce57740abb0a63db9e6c5bed490a115849
German Arevalo J <germanarevalojerez@gmail.com>
2021-10-27 17:46:49 -0500

    avances permisos gps

--- lib/pages/acceso_gps_page.dart
+++ lib/pages/acceso_gps_page.dart
@@ -1,14 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class AccesoGpsPage extends StatelessWidget {
+class AccesoGpsPage extends StatefulWidget {
   const AccesoGpsPage({Key? key}) : super(key: key);
+
+  @override
+  State<AccesoGpsPage> createState() => _AccesoGpsPageState();
+}
+
+class _AccesoGpsPageState extends State<AccesoGpsPage>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance?.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance?.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    if (state == AppLifecycleState.resumed) {
+      if (await Permission.location.isGranted) {
+        Navigator.pushReplacementNamed(context, 'loading');
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('AccesoGpsPage'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Es necesario el GPS para usar esta app'),
+            MaterialButton(
+                child: Text(
+                  'Solicitar acceso',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.black,
+                shape: StadiumBorder(),
+                elevation: 0,
+                splashColor: Colors.transparent,
+                onPressed: () async {
+                  final status = await Permission.location.request();
+                  this.accesoGps(status);
+                }),
+          ],
+        ),
       ),
     );
+  }
+
+  void accesoGps(status) {
+    print(status);
+    switch (status) {
+      case PermissionStatus.granted:
+        Navigator.pushReplacementNamed(context, 'mapa');
+        break;
+      case PermissionStatus.denied:
+      case PermissionStatus.restricted:
+      case PermissionStatus.permanentlyDenied:
+        openAppSettings();
+    }
   }
 }
