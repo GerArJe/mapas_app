ab831a8e317fa64fb20311fcd7720bd960909465
German Arevalo J <germanarevalojerez@gmail.com>
2021-10-29 09:45:30 -0500

    bloc busqueda

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:mapas_app/bloc/mapa/mapa_bloc.dart';
 import 'package:mapas_app/bloc/mi_ubicacion/mi_ubicacion_bloc.dart';
+import 'package:mapas_app/bloc/busqueda/busqueda_bloc.dart';
 import 'package:mapas_app/pages/loading_page.dart';
 import 'package:mapas_app/pages/mapa_page.dart';
 import 'package:mapas_app/pages/acceso_gps_page.dart';
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
       providers: [
         BlocProvider(create: (_) => MiUbicacionBloc()),
         BlocProvider(create: (_) => MapaBloc()),
+        BlocProvider(create: (_) => BusquedaBloc()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
